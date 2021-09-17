4724cf043361077f698fc057e476e132e616a526
priyalreja <84955865+priyalreja@users.noreply.github.com>
2021-09-17 18:25:58 +0530

    added gitignore file

---
+++ playwright.config.js
@@ -0,0 +1,23 @@
+const config = {
+  testDir: './tests/e2e',
+  use: {
+    headless: false,
+    viewport: { width: 1280, height: 720 },
+    ignoreHTTPSErrors: true,
+    video: 'on-first-retry',
+  },
+  projects: [
+    {
+      name: 'Desktop Firefox',
+      use: {
+        browserName: 'firefox',
+        viewport: { width: 800, height: 600 },
+      },
+    },
+  ],
+  expect: {
+    toMatchSnapshot: { threshold: 0.8 },
+  },
+};
+
+module.exports = config;
