ae79b9ba0e170adbbf1c5207e73822d5e985f271
slysetti <slysetti@seismic.com>
2021-09-25 18:58:00 +0530

    added changes to extract all links by levels

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var commands = require('minimist')(process.argv.slice(2));
 const linkExtractor = require('./linkExtractor');
 
-linkExtractor.getLinks('https://playwright.dev');
+linkExtractor.getLinks('https://0a15-115-248-53-14.ngrok.io/');
 
 
 
--- linkExtractor.js
+++ linkExtractor.js
@@ -1,34 +1,59 @@
 // @ts-check
 const playwright = require('playwright');
-
-async function getLinks (baseurl)
- {
+async function getLinks(baseurl) {
   const browser = await playwright.chromium.launch()
   const page = await browser.newPage()
+  const urlsWithLevel = new Array([baseurl]);
   const seenURLs = new Set()
-  const crawl = async (url) => {
-
+  const crawl = async (url, level) => {
     if (seenURLs.has(url)) {
       return
     }
-    seenURLs.add(url)
+
+    seenURLs.add(url);
+
     if (!url.startsWith(baseurl)) {
       return
     }
-    console.log(`Visiting ${url}\n`)
-    await page.goto(url)
-    const urls = await page.$$eval('a', (elements) =>
-      elements.map((el) => el.href),
-    )
-    for await (const u of urls) {
-      console.log(u);
-      await crawl(u);
+    const links = await extractLinks(url);
+    console.log(`Visiting ${url} link count${links.length}`)
+
+    if (links.length > 0) {
+      if (urlsWithLevel.length > 0 && urlsWithLevel[level] != undefined)
+        urlsWithLevel[level] = [...urlsWithLevel[level], ...links];
+      else
+        urlsWithLevel[level] = links;
+    }
+
+    for (let index = 0; index < links.length; index++) {
+      const link = links[index];
+      await crawl(link, level + 1);
     }
   }
 
-  await crawl(baseurl)
-  console.log(`Checked ${seenURLs.size} URLs`)
+  const extractLinks = async (url) => {
+    await page.goto(url, { timeout: 0 })
+    const urls = await page.$$eval('a', (elements) =>
+      elements.map((el) => el.href),
+    )
+    return urls;
+  }
+
+  await crawl(baseurl, 1);
+
+  // console.log(`Checked ${seenURLs.size} URLs`)
+  // seenURLs.forEach(url => {
+  //   console.log(url);
+  // });
+  // console.log(`total levels found ${urlsWithLevel.length}`)
+  // for (let index = 0; index < urlsWithLevel.length; index++) {
+  //   console.log(`for level ${index} =>`);
+  //   urlsWithLevel[index].forEach(url => {
+  //     console.log(url);
+  //   });
+  // }
   await browser.close()
+  return urlsWithLevel;
 }
 
 module.exports = { getLinks };
