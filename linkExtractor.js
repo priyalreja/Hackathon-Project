f9260d85a932272a08171c82f2efd83df1bcaafd
slysetti <slysetti@seismic.com>
2021-09-25 15:52:19 +0530

    Exported getLinks func

--- linkExtractor.js
+++ linkExtractor.js
@@ -1,29 +1,34 @@
+// @ts-check
 const playwright = require('playwright');
 
-(async () => {
+async function getLinks (baseurl)
+ {
   const browser = await playwright.chromium.launch()
   const page = await browser.newPage()
   const seenURLs = new Set()
   const crawl = async (url) => {
+
     if (seenURLs.has(url)) {
       return
     }
     seenURLs.add(url)
-    if (!url.startsWith('https://playwright.dev')) {
+    if (!url.startsWith(baseurl)) {
       return
     }
-    console.log(`Visiting ${url}`)
+    console.log(`Visiting ${url}\n`)
     await page.goto(url)
-
     const urls = await page.$$eval('a', (elements) =>
       elements.map((el) => el.href),
     )
     for await (const u of urls) {
       console.log(u);
-      await crawl(u)
+      await crawl(u);
     }
   }
-  await crawl('https://playwright.dev')
+
+  await crawl(baseurl)
   console.log(`Checked ${seenURLs.size} URLs`)
   await browser.close()
-})()
+}
+
+module.exports = { getLinks };
